0e2afa7bb39224e45c5e4532a1cc5f619e13fcb0
SamHwang1990 <SamHwang1990@gmail.com>
2014-05-18 18:15:56 +0800

    add search elements and add some behavior codes

--- ClientManage.WebUI/Scripts/apps/StudentMgr/Index/List/list_view.js
+++ ClientManage.WebUI/Scripts/apps/StudentMgr/Index/List/list_view.js
@@ -22,7 +22,32 @@ define([
 			className:"wrap",
 			template: _.template(StudentsTpl),
 			itemView:View.StudentItemView,
-			itemViewContainer: "tbody"
+			itemViewContainer: "tbody",
+			ui:{
+				"formSearch":".form-search",                //form 元素
+				"conditionCurrent":".condition-current",    //当前分类元素
+				"conditionList":".condition-list",          //分类列表
+				"searchContent":".search-content",          //筛选内容元素
+				"btnSubmit":"button[type=submit]"           //submit 按钮
+			},
+			events:{
+				'click @ui.conditionList a':'ConditionSortChange',      //搜索框交互
+				'submit':'SortCommit'
+			},
+			ConditionSortChange:function(event){
+				event.preventDefault();
+				var sortName = event.target.text;                       //获取分类名字
+				this.ui.conditionCurrent.html(sortName);                //修改当前分类显示
+				if($(event.target).hasClass('condition-all')){          //修改“全部分类”
+					this.ui.searchContent.attr('disabled','disabled');  //禁用输入框
+					this.ui.btnSubmit.attr('disabled','disabled');      //禁用搜索按钮
+					this.ui.searchContent.attr('placeholder','全部无需筛选');     //修改placeholder
+				}else{
+					this.ui.searchContent.removeAttr('disabled');       //
+					this.ui.btnSubmit.removeAttr('disabled');
+					this.ui.searchContent.attr('placeholder','请输入筛选的'+sortName);
+				}
+			}
 
 		});
 	});
