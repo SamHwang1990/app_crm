878c61088792287e88e2d48a6859dd71adab47a8
SamHwang1990 <SamHwang1990@gmail.com>
2014-09-01 15:33:08 +0800

    change the studen list module to bootstrap table plugin

--- ClientManage.WebUI/Scripts/apps/StudentMgr/Index/List/list_view.js
+++ ClientManage.WebUI/Scripts/apps/StudentMgr/Index/List/list_view.js
@@ -9,8 +9,10 @@ define([
 	'text!templates/StudentMgr/Index/List.html',
 	'models/StudentMgr/EnumModel/EducationIntention',
 	'models/StudentMgr/EnumModel/Grade',
-	'models/StudentMgr/EnumModel/Gender'
-	],function(ClientManage,StudentItemTpl,StudentsTpl,EnumEducationIntention,EnumGrade,EnumGender){
+	'models/StudentMgr/EnumModel/Gender',
+	'models/StudentMgr/EnumModel/IsSign',
+	"BootstrapTable"
+	],function(ClientManage,StudentItemTpl,StudentsTpl,EnumEducationIntention,EnumGrade,EnumGender,EnumIsSign,BootstrapTable){
 	ClientManage.module('StudentMgr.Index.List.View',function(View,ClientManage,Backbone, Marionette, $, _){
 		View.StudentItemView = Marionette.ItemView.extend({
 			template:_.template(StudentItemTpl),
@@ -42,25 +44,28 @@ define([
 				}
 			}
 		});
-		View.StudentsView = Marionette.CompositeView.extend({
+
+		View.StudentsView = Marionette.ItemView.extend({
 			tagName:"div",
 			className:"wrap",
 			template: _.template(StudentsTpl),
-			itemView:View.StudentItemView,
-			itemViewContainer: "tbody",
-			/*initialize:function(){
-				_.bindAll(this,'SortCommit');
-			},*/
 			ui:{
 				"formSearch":".form-search",                //form 元素
 				"conditionCurrent":".condition-current",    //当前分类元素
 				"conditionList":".condition-list",          //分类列表
 				"searchContent":".search-content",          //筛选内容元素
-				"btnSubmit":"button[type=submit]"           //submit 按钮
+				"btnSubmit":"button[type=submit]" ,         //submit 按钮
+				"tableStudentList":"table#StudentListTable"
 			},
 			events:{
 				'click @ui.conditionList a':'ConditionSortChange',      //搜索框交互
 				'submit':'SortCommit'
+			},
+			onRender:function(){
+				this.CollectionHelpers();
+			},
+			onShow:function(){
+				this.RenderBootstrapTable();
 			},
 			ConditionSortChange:function(event){
 				event.preventDefault();
@@ -110,6 +115,140 @@ define([
 						}
 					})
 				}
+			},
+			CollectionHelpers:function(){
+				_.each(this.collection.models,function(studentItem){
+					var studentInfo = studentItem.get("StudentInfo");
+					studentInfo.EducationIntention = EnumEducationIntention.EducationIntentionInverse[studentInfo.EducationIntention];
+					studentInfo.Grade = EnumGrade.GradeInverse[studentInfo.Grade];
+					studentInfo.Gender = EnumGender.GenderInverse[studentInfo.Gender];
+				})
+			},
+			SetTableColumns:function(){
+				var columnColumns = [
+					{field: 'studentName', title: '名字', sortable: true},
+					{field: 'gender', title: '性别',sortable: true},
+					{field: 'schoolCn', title: '在读学校',align:"center",formatter:function(value){
+						if (!value) {
+							return '-';
+						}
+
+						if(value.schoolCn == null){
+							return "-";
+						}else if(value.schoolCn.indexOf('其他') >= 0)
+							return value.otherSchool;
+						else
+							return value.schoolCn;
+					}},
+					{field: 'educationIntention', title: '年级', sortable:true},
+					{field: 'saleConsultantName', title: '销售负责人', sortable:true},
+					{field: 'studentIsSign', title: '签约状态',sortable: true,formatter:function(value){
+						if (!value) {
+							return '-';
+						}
+						return EnumIsSign.IsSignInverse[value.IsSign];
+					}},
+					{field: 'exec', title: '操作', formatter: function (value) {
+						if (!value) {
+							return '-';
+						}
+						var execArray = [];
+						execArray.push('<a href="#StudentMgr/Index/Edit/Student-' +
+							value.StudentInfo.StudentID +
+							'" title="' +
+							value.StudentInfo.NameCn +
+							'">修改基本信息</a>');
+
+						execArray.push('<a href="#StudentMgr/Index/Edit/Contacts-' +
+							value.StudentInfo.StudentID +
+							'" title="' +
+							value.StudentInfo.NameCn +
+							'">修改联系人信息</a>');
+
+						if (value.AppRelation.IsSign == '4' ) {
+							if (!value.AppRelation.HasAssignConsultant && !value.AppRelation.HasScheduleApply) {
+								execArray.push('<a href="#StudentMgr/AssignConsultant-'
+									+ value.StudentInfo.StudentID
+									+ '" title="'
+									+ value.StudentInfo.NameCn
+									+ '" class="btn btn-info btn-mini">分配顾问</a>')
+							} else if (value.AppRelation.HasAssignConsultant && !value.AppRelation.HasScheduleApply){
+								execArray.push('<a href="#StudentMgr/ScheduleApply-'
+									+ value.StudentInfo.StudentID
+									+ '" title="'
+									+ value.StudentInfo.NameCn
+									+ '" class="btn btn-info btn-mini">安排申请时间表</a>')
+							} else {
+								execArray.push('<a href="#StudentMgr/Apply/'
+									+ value.StudentInfo.StudentID
+									+ '" title="'
+									+ value.StudentInfo.NameCn
+									+ '" class="btn btn-success btn-mini">进入申请</a>')
+							}
+
+						} else {
+							execArray.push('<a href="#StudentMgr/SaleTrack/AppInterview-'
+								+ value.StudentInfo.StudentID
+								+ '" title="'
+								+ value.StudentInfo.NameCn
+								+ '" class="btn btn-info btn-mini">进入销售</a>')
+						}
+
+						return execArray.join('&emsp;');
+					}}
+				];
+				return columnColumns;
+			},
+			SetTableData:function(){
+				var tableData = [];
+				_.each(this.collection.models,function(studentItem){
+					var dataItem = {};
+					var studentInfo = studentItem.get("StudentInfo");
+					var appRelation = studentItem.get("AppRelation");
+
+					dataItem["studentName"] = studentInfo.NameCn
+					dataItem["gender"] = studentInfo.Gender;
+					dataItem["schoolCn"] = {
+						schoolCn:studentInfo.SchoolCn,
+						otherSchool:studentInfo.OtherSchool
+					}
+					dataItem["educationIntention"] = studentInfo.EducationIntention;
+					dataItem["saleConsultantName"] = appRelation.SaleConsultantName;
+					dataItem["studentIsSign"] = {
+						IsSign:appRelation.IsSign
+					};
+					dataItem["exec"] = {
+						StudentInfo:studentInfo,
+						AppRelation:appRelation
+					}
+					tableData.push(dataItem);
+				})
+				return tableData;
+			},
+			RenderBootstrapTable:function(){
+				var listView = this;
+				this.ui.tableStudentList.bootstrapTable({
+					data:listView.SetTableData(),
+					columns:listView.SetTableColumns(),
+					striped: true,
+					pagination: true,
+					sidePagination:"client",
+					pageSize: 10,
+					pageList: [10, 25, 50, 100, 200],
+					search: true,
+					showColumns: true,
+					minimumCountColumns: 2,
+					formatLoadingMessage:function(){
+						return '数据加载中，请稍后！'
+					},
+					formatRecordsPerPage:function(pageNumber){
+						return pageNumber + '条每页';
+					},
+					formatShowingRows:function(pageFrom, pageTo, totalRows){
+						//Showing %s to %s of %s rows
+						return '第&nbsp;' + pageFrom + '&nbsp;条到第&nbsp;' + pageTo + '&nbsp;条共&nbsp;' + totalRows + '&nbsp;条&emsp;';
+					}
+				});
 			}
 
 		});
